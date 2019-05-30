a9fb895a7e747cfb3e5739852debaf573aa1ffd9
Autumn <auresell1993@gmail.com>
2019-05-30 09:18:11 -0400

    Updated some javascript and commented out a TON of code--refactoring

--- codeLouisvilleProject_AutumnSellers/js/script.js
+++ codeLouisvilleProject_AutumnSellers/js/script.js
@@ -1,5 +1,15 @@
+$("#aboutSection").hide();
+$("#portfolioSection").hide();
+$("#contactSection").hide();
+
 // Sets the background of the opening div based upon the time of day
-window.setInterval(setImage(), 5000)
+
+
+window.onresize = function(){
+  location.reload();
+}
+
+setTimeout(setImage(), 3000);
 
 function setImage(){
   var nowDate = new Date();
@@ -16,45 +26,42 @@ if(nowDate > wakeTime && nowDate < bedTime){
       document.getElementById('nightTime-link').style.display = "none";
       document.body.style.background = "url('./img/louisvilleDaytime.jpeg') center";
       document.body.style.backgroundSize = "cover";
-};
+      };
 }
 
-  // Control navigation, jump to carousel menu
-  // This function is not needed for desktop view so the media query
-  // makes it not active in screens larger than large tablets.
-
+// Control navigation, jump to carousel menu
+// This function is not needed for desktop view so the media query
+// makes it not active in screens larger than large tablets.
   if(window.matchMedia("(max-width: 900px)").matches){
     $('body').on('touchend click', ".louisvilleImage", function(){
     $(location).attr('href', '#contact_me')
-  });
+    });
   };
 
 //Allows the navigation menu to smoothly display at 800px and above
-window.onresize = function(){
 if(window.matchMedia("(min-width: 800px)").matches){
   $('body').ready(function(){
     $('#navigation-container').fadeIn('slow')
     });
-  }
-};
+}
+
 
 // Controls the displaying of the menu information
 // Controls the displaying of the menu information
 $("#about").on("click", function(){
-  $("#aboutSection").toggleClass("hide information-wrapper");
-  $("#aboutSection").addClass("information-wrapper")
-  $("#portfolioSection").addClass("hide");
-  $("#contactSection").addClass("hide");
+  $("#aboutSection").toggle();
+  $("#portfolioSection").hide();
+  $("#contactSection").hide();
 });
+
 $("#portfolio").on("click", function(){
-  $("#portfolioSection").toggleClass("no_show information-wrapper");
-  $("#portfolioSection").addClass("information-wrapper")
-  $("#aboutSection").addClass("no_show");
-  $("#contactSection").addClass("no_show");
+  $("#portfolioSection").toggle();
+  $("#aboutSection").hide();
+    $("#contactSection").hide();
 });
+
 $("#contact").on("click", function(){
-  $("#contactSection").toggleClass("no_show information-wrapper");
-  $("#contactSection").addClass("information-wrapper")
-  $("#aboutSection").addClass("no_show");
-  $("#portfolioSection").addClass("no_show");
+  $("#contactSection").toggleClass("hide_media_querie information-wrapper");
+  $("#aboutSection").addClass("hide_media_querie");
+  $("#portfolioSection").addClass("hide_media_querie");
 });
